d371c498765b4d96aa6467d24d7bfe8cde24be9d
Bill Duff <wduff@andrew.cmu.edu>
2012-03-23 21:40:57 -0400

    wtf

--- tetris.py
+++ tetris.py
@@ -1,1 +1,16 @@
-lol = 0
+import sys
+import commlib
+import host
+import client
+
+try:
+    if sys.argv[1] == "-c":
+        client.tetris(sys.argv[2])
+    elif sys.argv[1] == "-h":
+        host.tetris(int(sys.argv[2]))
+    elif sys.argv[1] == "-w":
+        print commlib.ipwords
+    else:
+        print "Usage: python tetris.py -c <room name> OR python tetris.py -h <number of clients> OR python tetris.py -w"
+except:
+    print "Usage: python tetris.py -c <room name> OR python tetris.py -h <number of clients> OR python tetris.py -w"
