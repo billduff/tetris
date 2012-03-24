b692dc37786abc8c4874adf52469630340a41e39
Eddie Nolan <eddiejnolan@gmail.com>
2012-03-23 20:54:07 -0400

    updated commlib

---
+++ commlib.py
@@ -0,0 +1,17 @@
+class Commlink(object):
+	def getThisIP(self):
+		print "This function should return the IP address of the computer"
+	def connectToIP(self, ip):
+		print "This function should connect to a different computer"
+	def send(self,ip,message):
+		print "This function should send a message to the other IP"
+	def poll(self):
+		print "This function should get messages from the other computers"
+
+ipWords = [[]]
+
+def ipToWords(ip):
+	pass
+
+def wordsToIP(words):
+	pass
